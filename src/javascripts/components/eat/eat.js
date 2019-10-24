fc8a6c5e0032414ff13f381e03dae7c415b87a40
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-10-24 15:15:09 -0500

    added logic to print eat qudrant bar and buttons

--- src/javascripts/components/eat/eat.js
+++ src/javascripts/components/eat/eat.js
@@ -1,7 +1,14 @@
 import './eat.scss';
+import utilities from '../../utilities/utilities';
 
-const printEatButtons = () => {
-  console.error('hi');
+const printEatQuad = () => {
+  let domString = '';
+  const full = 100;
+  domString += `<div class="eat-buttons">
+  <p><progress value="${full}" max="100" id="eatTotal">Full</progress></p>
+  <p><button class='primary' id='healthy'>Granola</button> <button class ='primary' id='junk'>Cheetos</button></p>
+  </div>`;
+  utilities.printToDom('eat', domString);
 };
 
-export default { printEatButtons };
+export default { printEatQuad };
